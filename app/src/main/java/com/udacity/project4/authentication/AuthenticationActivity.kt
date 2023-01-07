f8f6633ff84fd20a830b55f2f2c159f9c0433c03
Haitham Mahmoud <m.haitham2001@gmail.com>
2023-01-07 20:25:49 +0200

    first commit

---
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -0,0 +1,37 @@
+package com.udacity.project4.authentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.udacity.project4.R
+
+/**
+ * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
+ * signed in users to the RemindersActivity.
+ */
+class AuthenticationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+
+//          TODO: If the user was authenticated, send him to RemindersActivity
+
+//          TODO: a bonus is to customize the sign in flow to look nice using :
+        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+
+    }
+    private fun launchSignInFlow() {
+        // Types of mails you can sign in with
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
+            77//SIGN_IN_RESULT_CODE
+        )
+    }
+}
